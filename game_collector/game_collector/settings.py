e4b06a17e29df9fcfb0d811b3c6147d93fd9451c
Aaron <aaron@JP1712PC39A.local>
2020-05-18 22:41:34 +0800

    save

--- game_collector/game_collector/settings.py
+++ game_collector/game_collector/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'dist/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'game-frontend', 'webpack-stats.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json'),
     }
 }
